831dcf57ad457c4b068c3852381141b3bd1ac2ed
0gi-h <103099496+0gi-h@users.noreply.github.com>
2025-08-31 13:25:18 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 72 ms, Memory: 12604 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -7,14 +7,14 @@ public class Main {
    static int[][] map;
    static int MAX = 0;
    public static void main(String[] args) throws IOException {
-      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(in.readLine());
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
       N = Integer.parseInt(st.nextToken());
       M = Integer.parseInt(st.nextToken());
       D = Integer.parseInt(st.nextToken());
       map = new int[N][M];
       for(int i=0;i<N;i++) {
-         st = new StringTokenizer(in.readLine());
+         st = new StringTokenizer(br.readLine());
          for(int j=0;j<M;j++){
             map[i][j] = Integer.parseInt(st.nextToken());
          }
@@ -23,17 +23,27 @@ public class Main {
       System.out.println(MAX);
    }
  
-   public static void pick(int cnt, int start){
-      if(cnt==3){
+   public static void pick(int idx, int start){
+      if(idx==3){
          MAX = Math.max(MAX,game());
          return;
       }
-      for(int i=start;i<M;i++){
-         picked[cnt] = i;
-         pick(cnt+1, i+1);
+      if(start >= M){
+          return; // 더 이상 선택할 수 있는 인덱스가 없음
       }
+      
+      // 현재 start 인덱스를 선택하는 경우
+      picked[idx] = start;
+      pick(idx + 1, start + 1);
+      
+      // 현재 start 인덱스를 선택하지 않는 경우
+      pick(idx, start + 1);
    }
  
+	 //상태를 저장할 배열 status
+	 //궁수를 한 줄씩 올리는 방식(line)
+	 //거리 1~D까지 반복문으로 돌면서 attack 호출
+	 //잡으면 반환값을 count에 추가
    public static int game(){
       int count = 0;
       int[][] status = new int[N][M];
@@ -52,6 +62,10 @@ public class Main {
       return count;
    }
  
+	 //pick은 궁수가 배치된 열, +- d의 배열만 탐색
+	 //적의 위치(nn, nm), nn은 d-(Math.abs(nm - pick), 여기가 핵심
+	 //status배열이 0이면 아직 처치하지 않은 적, 현재 궁수의 행(line)을 저장
+	 //현재 line과 status배열의 값이 같다면 다른 궁수가 쏜 적
    public static int attack(int[][] status, int d, int line, int pick){
       int nn;
       for(int nm=pick-d;nm<=pick+d;nm++){
